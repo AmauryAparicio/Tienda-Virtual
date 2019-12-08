ee788a22cfeaebd8657c99f03f25a8d8ba6efc73
Amaury <amaurysunstar@gmail.com>
2019-12-08 17:56:50 -0600

    Dia 2, se creo el index con la base de HTML

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tienda de camisetas</title>
+</head>
+<body>
+    <!-- Header -->
+    <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="">
+        </div>
+    </header>
+
+    <!-- Menu -->
+
+    <!-- Sider -->
+
+    <!-- Content -->
+    
+    <!-- Footer -->
+    
+</body>
+</html>
