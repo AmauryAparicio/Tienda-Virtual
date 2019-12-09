d92fa40bb40a5d9f5454e5787e18752dbb019d38
Amaury <amaurysunstar@gmail.com>
2019-12-09 00:36:13 -0600

    Dia 2, se termino de maquetar el HTML y se inicio el prototipado con CSS

--- index.php
+++ index.php
@@ -5,22 +5,96 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tienda de camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-    <!-- Header -->
-    <header id="header">
-        <div id="logo">
-            <img src="assets/img/camiseta.png" alt="">
+    <div class="container">
+        <!-- Header -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                <a href="index.php">
+                    <h1>Tienda de Camisetas</h1>
+                </a>
+            </div>
+        </header>
+
+        <!-- Menu -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
+
+            <!-- Sider -->
+            <aside id="lateral">
+                
+                <div class="block_aside" id="login">
+                    <!-- Formulario de login -->
+                    <form action="#" method="POST">
+                        <!-- email -->
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email">
+                        <!-- Contraseña -->
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="">
+                        <input type="submit" value="Enviar">
+                    </form>
+
+                    <a href="#">Mis pedidos</a> 
+                    <a href="#">Gestionar pedidos</a>
+                    <a href="#">Gestionar categorias</a>
+                </div>
+            </aside>
+
+            <!-- Content -->
+            <div id="central">
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 DLS</p>
+                    <a href="#">Comprar</a>
+                </div>
+
+                <div class="product">
+                        <img src="assets/img/camiseta.png">
+                        <h2>Camiseta Azul Ancha</h2>
+                        <p>30 DLS</p>
+                        <a href="#">Comprar</a>
+                    </div>
+
+                    <div class="product">
+                            <img src="assets/img/camiseta.png">
+                            <h2>Camiseta Azul Ancha</h2>
+                            <p>30 DLS</p>
+                            <a href="#">Comprar</a>
+                        </div>
+            </div>
         </div>
-    </header>
 
-    <!-- Menu -->
-
-    <!-- Sider -->
-
-    <!-- Content -->
-    
-    <!-- Footer -->
-    
+        <!-- Footer -->
+        <footer id="footer">
+            <p>Desarrollado por Amaury Aparicio &copy; <?=date('Y')?></p>
+        </footer>
+    </div>
 </body>
 </html>
