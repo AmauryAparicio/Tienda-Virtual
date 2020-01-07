4d4b1db715bd0ba0eee596e51e667b57a937e3c6
Amaury <amaurysunstar@gmail.com>
2020-01-06 23:51:35 -0600

    X

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 //parametros especiales
-define("base_url", "http://localhost/Proyecto2PHP/");
+define("base_url", "http://localhost/Tienda-Virtual/");
 define("controller_default", "productoController");
 define("action_default", "index");
